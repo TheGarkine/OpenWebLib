46a8b9168e1cc4919fe575cdf3c8b96505dcab31
Dino Pehratovic <dino.pehratovic@outlook.de>
2020-02-11 20:32:34 +0100

    Added language detection for the OCR-scanner and added gitignore

--- indexing/src/pdf_processing/pdf_parser/pdf_parser.py
+++ indexing/src/pdf_processing/pdf_parser/pdf_parser.py
@@ -2,34 +2,35 @@ import PyPDF2
 import pytesseract
 from pdf2image import convert_from_path
 import tempfile
+from langdetect import detect # Documentation: https://pypi.org/project/langdetect/
+
+
+def detect_language(sample_text):
+    lang = detect(sample_text)
+    print("Detected language: {}".format(lang))
+    return "deu" if lang == "de" else "eng"
 
 
 def extract_pdf_text(file_path):
-
-    # Converts all pages of the PDF-file into JPG-files
     print("Starting to transform the received PDF-file into JPG-files")
     with tempfile.TemporaryDirectory() as path:
+        # Converts all pages of the PDF-file into JPG-files
         images = convert_from_path(file_path, output_folder=path, dpi=300, fmt=".jpg")
-            
         print("Finished transforming the PDF-file into image-files")
+        # Detects which language is used within the PDF-file
+        print("Trying to detect whether the PDF is written in English or German")
+        orc_language = detect_language(pytesseract.image_to_string(images[0], lang="eng"))
 
         # Iterates through all images and retrieves the text
         print("Starting to process the image-files with the OCR-scanner")
         full_text = ""
         for image in images:
             print("Starting to process: {}".format(image))
-            text = pytesseract.image_to_string(image, lang="eng")
+            text = pytesseract.image_to_string(image, lang=orc_language)
             full_text += text + "\n"
-
-        print("Finished finished processing of {}".format(image))
-        print("--------------------------------------")
+            print("Finished processing of {}".format(image))
+            print("--------------------------------------")
+        print("All images are processed successfully")
     return full_text
 
 
-# Problem: OCR-Scanner ist in der Lage alle möglichen Texte zu parsen, jedoch muss er wissen in welcher Sprache der Text verfasst ist
-# Das Problem hierbei ist nicht die Sprache zu erkennen, sondern alle möglichen Sprachmodelle installiert zu haben. Es sind ziemlich viele
-# Auf welche Sprachen wollen wir uns beschränken?
-# Dokumentation: https://pypi.org/project/langdetect/
-#from langdetect import detect, detect_langs
-#lang1 = detect("Ein, zwei, drei, vier")
-#lang2 = detect("One two three four")
